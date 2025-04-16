9aed87e1d08350bff918003362f847ce26aae6d0
Raúl Ferreyra <raulrferreyrav@gmail.com>
2025-04-16 16:08:52 -0500

    New Animation

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -161,6 +161,23 @@ header h1 {
     }
 }
 
+.home-init,
+#settings {
+    animation: fadeInUp 0.6s ease both;
+}
+
+@keyframes fadeInUp {
+    from {
+        opacity: 0;
+        transform: translateY(30px);
+    }
+
+    to {
+        opacity: 1;
+        transform: translateY(0);
+    }
+}
+
 #settings {
     display: flex;
     flex-direction: column;
