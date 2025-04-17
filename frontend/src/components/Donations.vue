b133f47e5e02d3846edbfab58f2ec04529517d7b
Raúl Ferreyra <raulrferreyrav@gmail.com>
2025-04-17 18:10:21 -0500

    Init Overflow

--- frontend/src/components/Donations.vue
+++ frontend/src/components/Donations.vue
@@ -1,5 +1,26 @@
 <template>
-    PayMe Please
+    <div class="overflow">
+        <div class="frame">
+            <div class="message">
+                <h2 class="title">Si te gusta mi herramienta</h2>
+                <h3 class="subtitle">por favor apóyame</h3>
+                <a href="https://" target="_blank" rel="noopener noreferrer">Por Plin (Solo Perú)</a>
+                <a href="https://" target="_blank" rel="noopener noreferrer">PayPal (Otros Países)</a>
+            </div>
+        </div>
+    </div>
 </template>
 
-<style lang="css" scoped></style>
+<style lang="css" scoped>
+.overflow {
+    position: absolute;
+    top: 0;
+    left: 0;
+    background: rgba(200, 200, 200, .2);
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    display: flex;
+    transition: all .3s ease-in-out;
+}
+</style>
