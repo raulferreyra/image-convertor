14a44fb88a7b96053b3c5378d38f1acaf698f908
Raúl Ferreyra <raulrferreyrav@gmail.com>
2025-04-22 11:12:47 -0500

    Oh my... important fix

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,23 +1,11 @@
 <script setup>
+import { ref } from 'vue'
 import Home from './components/Home.vue'
 import Donations from './components/Donations.vue'
+const overflowStage = ref(false)
 </script>
 
 <template>
-  <Home :overflowStage="overflowStage"2 />
+  <Home :overflowStage="overflowStage" />
   <Donations :overflowStage="overflowStage" />
 </template>
-
-<script>
-export default {
-  components: {
-    Home,
-    Donations
-  },
-  data() {
-    return {
-      overflowStage: false
-    }
-  },
-}
-</script>
