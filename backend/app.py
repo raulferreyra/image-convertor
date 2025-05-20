97452a2ad8410d5f62b598bfef1c3d1e01db7968
Raúl Ferreyra <raulrferreyrav@gmail.com>
2025-05-20 14:36:31 -0500

    Update App.py width conection

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,10 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template
 from convert import ImageCompressor, ImageConverter
 import os
 import tempfile
 import shutil
 
 app = Flask(__name__)
-
-
-@app.route('/')
-def index():
-    return render_template('index.html')
 
 
 """
@@ -23,31 +18,43 @@ def convert():
     max_weight_mb = request.form.get('max_weight', '1')
     max_weight_bytes = int(float(max_weight_mb) * 1024 * 1024)
     target_format = request.form.get('lot_convert', 'webp')
+    dest_path = request.form.get('dest_path')
 
     # Get the redimension
     red_width = request.form.get('red_width')
     red_height = request.form.get('red_height')
 
     # Manage Files
-    if 'file_orig' not in request.files or 'file_dest' not in request.files:
-        return "Archivos no enviados correctamente", 400
+    if 'file_orig' not in request.files:
+        return "No se enviaron archivos correctamente", 400
 
     orig_folder = tempfile.mkdtemp()
-    dest_folder = tempfile.mkdtemp()
 
     # Extract folders
     for file in request.files.getlist("file_orig"):
-        file_path = os.path.join(orig_folder, file.filename)
+        rel_path = file.filename  # incluye subcarpetas si se subió así
+        file_path = os.path.join(orig_folder, rel_path)
         os.makedirs(os.path.dirname(file_path), exist_ok=True)
         file.save(file_path)
 
+    os.makedirs(dest_path, exist_ok=True)
+
     # Space for proccessing
     compressor = ImageCompressor(max_size=max_weight_bytes)
-    converter = ImageConverter(orig_folder, dest_folder, compressor)
+    converter = ImageConverter(
+        orig_folder, dest_path, compressor, format=target_format)
+
+    if red_width and red_height:
+        try:
+            converter.set_resize(int(red_width), int(red_height))
+        except ValueError:
+            return "Dimensiones inválidas", 400
+
     converter.convert_all()
+    shutil.rmtree(orig_folder)
 
     # Future ZIPS
-    return f"Conversión finalizada. Las imágenes están en: {dest_folder}"
+    return f"Conversión finalizada. Las imágenes están en: {dest_path}"
 
 
 if __name__ == "__main__":
